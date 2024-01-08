d86f4e7e15b4cf9c86f81ccc2b79bd668c0a2a09
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-01-08 18:01:29 -0500

    Revert virtual thread refactoring.
    Keep the AIC to `Runable` refactoring.

--- solr/core/src/test/org/apache/solr/cloud/TestStressInPlaceUpdates.java
+++ solr/core/src/test/org/apache/solr/cloud/TestStressInPlaceUpdates.java
@@ -159,7 +159,7 @@ public class TestStressInPlaceUpdates extends AbstractFullDistribZkTestBase {
 
     for (int i = 0; i < nWriteThreads; i++) {
       Thread thread =
-          Thread.ofVirtual().name("WRITER" + i).unstarted(() -> {
+          Thread.ofPlatform().name("WRITER" + i).unstarted(() -> {
             Random rand = new Random(random().nextInt());
               try {
                 while (operations.decrementAndGet() > 0) {
@@ -376,7 +376,7 @@ public class TestStressInPlaceUpdates extends AbstractFullDistribZkTestBase {
     // Read threads
     for (int i = 0; i < nReadThreads; i++) {
       Thread thread =
-          Thread.ofVirtual().name("READER" + i).unstarted(() -> {
+          Thread.ofPlatform().name("READER" + i).unstarted(() -> {
             Random rand = new Random(random().nextInt());
               try {
                 while (operations.decrementAndGet() >= 0) {
